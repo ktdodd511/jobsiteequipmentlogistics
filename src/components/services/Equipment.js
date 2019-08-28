22a3667c9081024306656b0113416d81dbd5d80f
katiedodd <ktdodd511@gmail.com>
2019-08-28 11:02:17 -0700

    fixing dependency issue

--- src/components/services/Equipment.js
+++ src/components/services/Equipment.js
@@ -6,7 +6,8 @@ class Equipment extends Component {
   render() {
     return (
       <div>
-        <h2 className="text-center"><i className="fas fa-hard-hat fa-2x" /> Equipment Rental</h2>
+        <h2 className="text-center"><i className="fas fa-hard-hat fa-2x" /></h2>
+        <h2 className="text-center"> Equipment Rental</h2>
         <div class="container mt-4 services-text">
           <h3 className="text-center">From a single pallet jack to scissor lifts – if you need it we can get it.</h3>
           <h3 className="text-center"><Link to="/contact"> Contact Us to Learn More </Link></h3>
--- src/components/services/Recycling.js
+++ src/components/services/Recycling.js
@@ -6,7 +6,8 @@ class Recycling extends Component {
   render() {
     return (
       <div>
-        <h2 className="text-center"><i className="fas fa-recycle fa-2x" /> Recycling</h2>
+        <h2 className="text-center"><i className="fas fa-recycle fa-2x" /></h2>
+        <h2 className="text-center"> Recycling</h2>
         <div class="container mt-4 services-text">
           <h3 className="text-center">Customized waste & recycling plans for your projects. JEL can design a compliant waste plan for LEED, Calgreen or other green building initiatives.</h3>
           <h3 className="text-center"><Link to="/contact"> Contact Us to Learn More </Link></h3>
